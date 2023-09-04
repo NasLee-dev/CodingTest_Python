0fcf90d0b22c01344c6db0ed72b1aa5c6e7b2003
fined000@naver.com <fined000@naver.com>
2023-09-04 09:35:58 +0900

    leetcode test

--- LeetCode/GenerateParentheses.js
+++ LeetCode/GenerateParentheses.js
@@ -1,3 +1,20 @@
 var generateParenthesis = function(n) {
-    
+  const result = [];
+  const generate = (left, right, str) => {
+    if (left === 0 && right === 0) {
+      result.push(str);
+      return;
+    }
+    if (left > 0) {
+      generate(left - 1, right, str + "(");
+    }
+    if (right > left) {
+      generate(left, right - 1, str + ")");
+    }
+  }
+  generate(n, n, "");
+  return result;
 };
+
+
+console.log(generateParenthesis(1));
