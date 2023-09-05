999d91b9c89ef224fd0e3e0a93db93decbe51e18
fined000@naver.com <fined000@naver.com>
2023-09-05 10:50:01 +0900

    leetcode test

---
+++ NextPermutation.js
@@ -0,0 +1,27 @@
+var nextPermutation = function(nums) {
+  const n = nums.length;
+  for (let i = n - 2; i >= 0; i--) {
+    if(nums[i] < nums[i+1]) {
+      for (let j = n - 1; j > i; j--) {
+        if(nums[j] > nums[i]) {
+          swap(nums, i, j);
+          reverse(nums, i + 1, n - 1);
+          return;
+        }
+      }
+    }
+  }
+  nums.reverse();
+};
+
+const swap = (nums, i, j) => ([nums[i], nums[j]] = [nums[j], nums[i]]);
+
+const reverse = (nums, start, end) => {
+  while (start < end) {
+    swap(nums, start++, end--);
+  }
+};
+
+console.log(nextPermutation([1,2,3]));  
+
+// Very Hard.
