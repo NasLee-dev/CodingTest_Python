0fe12092f955af6951c289d27bdf4a77d0e226bb
fined000@naver.com <fined000@naver.com>
2023-09-14 11:45:54 +0900

    add test

--- LeetCode/Medium/CombinationSum.js
+++ LeetCode/Medium/CombinationSum.js
@@ -19,3 +19,24 @@ var combinationSum = function(candidates, target) {
 
 console.log(combinationSum([2,3,6,7], 7));
 
+var comSum = function(candidates, target) {
+  let res = [];
+  let temp = [];
+
+  let iterate = (index, sum) => {
+    if (sum > target) return;
+    if (sum === target) {
+      res.push([...temp]);
+      return;
+    }
+    for (let i = index; i < candidates.length; i++) {
+      if (candidates[i] > target) continue;
+      temp.push(candidates[i]);
+      iterate(i, sum + candidates[i]);
+      temp.pop();
+    }
+  }
+  iterate(0, 0);
+  return res;
+};
+
